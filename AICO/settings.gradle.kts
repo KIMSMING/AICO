7545ce47005e795a3732ffa54514a8c04e162d12
minwns11 <65673393+minwns11@users.noreply.github.com>
2025-05-08 10:57:55 +0900

    네이버 로그인 sdk 쑤정

--- AICO/settings.gradle.kts
+++ AICO/settings.gradle.kts
@@ -16,7 +16,6 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
-        maven { url = uri("https://naver.github.io/naveridlogin/android/sdk") }  // 네이버 SDK 저장소
         maven { url = uri("https://devrepo.kakao.com/nexus/content/groups/public/") }  // 카카오 SDK 저장소
     }
 }
