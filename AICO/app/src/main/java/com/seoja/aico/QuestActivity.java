4b844475f912f639d93cbbe41364fb05221555f1
KIMSMING <alstjdrla78@gmail.com>
2025-05-14 10:23:45 +0900

    질문하기 example 액티비티만 생성

--- AICO/app/src/main/java/com/seoja/aico/QuestActivity.java
+++ AICO/app/src/main/java/com/seoja/aico/QuestActivity.java
@@ -1,8 +1,6 @@
 package com.seoja.aico;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,32 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-
-    Button btnLogin, btnQuest;
+public class QuestActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quest);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-
-        btnLogin = findViewById(R.id.btnGoLogin);
-        btnQuest = findViewById(R.id.btnQuest);
-
-        btnLogin.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, com.seoja.aico.user.LoginActivity.class);
-            startActivity(intent);
-        });
-
-        btnQuest.setOnClickListener(v -> {
-            Intent intent = new Intent(MainActivity.this, QuestActivity.class);
-            startActivity(intent);
         });
     }
 }
